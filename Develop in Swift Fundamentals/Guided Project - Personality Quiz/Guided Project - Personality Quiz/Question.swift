dc47830e6cd64d7245836cbb7e4fed06b3e0f6fc
Alumno <alumno@MacBook-Pro-27.local>
2023-02-03 15:14:56 -0600

    Quiz

--- Develop in Swift Fundamentals/Guided Project - Personality Quiz/Guided Project - Personality Quiz/Question.swift
+++ Develop in Swift Fundamentals/Guided Project - Personality Quiz/Guided Project - Personality Quiz/Question.swift
@@ -24,5 +24,18 @@ struct Answer {
 
 enum AnimalType: Character {
     case dog = "🐶", cat = "🐱", rabbit = "🐰", turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends"
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy doing things on yout own terms."
+        case .rabbit:
+            return "You love everything that's soft. You are healthy and full of energy."
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+    }
 }
 
