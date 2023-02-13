8b76a90174b0e24fb4f90da46265cb64c4d5a6fe
alumno <alumno@MacBook-28.local>
2023-02-13 12:39:48 -0600

    Calculadora

---
+++ Objective C/Calculadora/Calculadora/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Calculadora
+//
+//  Created by alumno on 13/02/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
