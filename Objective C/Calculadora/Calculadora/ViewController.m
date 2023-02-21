35b9bd21cf642ce6a3ea7489afd0567b497e1444
alumno <alumno@MacBook-Pro-27.local>
2023-02-20 18:47:44 -0600

    calc

--- Objective C/Calculadora/Calculadora/ViewController.m
+++ Objective C/Calculadora/Calculadora/ViewController.m
@@ -11,15 +11,16 @@
     NSString *value1;
     NSString *value2;
     NSString *operation;
-    int result;
-    float resultD;
+    double result;
+    double resultD;
 }
 - (IBAction)numberButton:(UIButton*)sender;
 - (IBAction)operation:(UIButton*)sender;
 - (IBAction)clearButton:(UIButton*)sender;
 - (IBAction)resultButton:(UIButton*)sender;
+- (IBAction)dotButton:(UIButton *)sender;
+
 @property (weak, nonatomic) IBOutlet UILabel *numbersLabel;
-@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *numberButtonOutlet;
 @end
 
 @implementation ViewController
@@ -34,33 +35,52 @@
     _numbersLabel.text = @"";
 }
 
+- (IBAction)dotButton:(UIButton *)sender {
+    NSString *text;
+    if([_numbersLabel.text isEqual:(@"")]){
+        _numbersLabel.text = @"0";
+        text = [_numbersLabel.text stringByAppendingString:sender.titleLabel.text];
+            _numbersLabel.text = text;
+        if([_numbersLabel.text containsString:@"."]){
+            _numbersLabel.text = _numbersLabel.text;
+        }
+    }else{
+        if([_numbersLabel.text containsString:@"."]){
+            _numbersLabel.text = _numbersLabel.text;
+        }else{
+            text = [_numbersLabel.text stringByAppendingString:sender.titleLabel.text];
+            _numbersLabel.text = text;
+        }
+    }
+}
+
 - (IBAction)resultButton:(UIButton*)sender {
     if([operation isEqual:(@"+")]){
         value2 = _numbersLabel.text;
         _numbersLabel.text = @"";
-        result = value1.intValue + value2.intValue;
+        result = value1.doubleValue + value2.doubleValue;
         _numbersLabel.text = [@(result) stringValue];
     }
     if([operation isEqual:(@"-")]){
         value2 = _numbersLabel.text;
         _numbersLabel.text = @"";
-        result = value1.intValue - value2.intValue;
+        result = value1.doubleValue - value2.doubleValue;
         _numbersLabel.text = [@(result) stringValue];
     }
     if([operation isEqual:(@"×")]){
         value2 = _numbersLabel.text;
         _numbersLabel.text = @"";
-        result = (value1.intValue * value2.intValue);
+        result = (value1.doubleValue * value2.doubleValue);
         _numbersLabel.text = [@(result) stringValue];
     }
     if([operation isEqual:(@"÷")]){
         value2 = _numbersLabel.text;
         _numbersLabel.text = @"";
-        if (value2 == 0){
+        if (value2.doubleValue == 0){
             _numbersLabel.text = @"Error";
         }
         else{
-            resultD = (value1.intValue / value2.intValue);
+            resultD = value1.doubleValue / value2.doubleValue;
             _numbersLabel.text = [@(resultD) stringValue];
         }
     }
@@ -102,8 +122,12 @@
     if([_numbersLabel.text isEqual:(@"")]){
         _numbersLabel.text = sender.titleLabel.text;
     }else{
-        text = [_numbersLabel.text stringByAppendingString:sender.titleLabel.text];
-            _numbersLabel.text = text;
+        if([_numbersLabel.text isEqual:(@"0")]){
+            _numbersLabel.text = @"0";
+        }else{
+            text = [_numbersLabel.text stringByAppendingString:sender.titleLabel.text];
+                _numbersLabel.text = text;
+        }
     }
 }
 
